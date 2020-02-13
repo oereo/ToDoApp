a9f5e813cefa979f87b402b7e0502f1c9ab33023
oereo <dlstpgns0406@gmail.com>
2020-02-14 06:35:48 +0900

    add the bootstrap

--- to_dolist/src/App.vue
+++ to_dolist/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+     <router-view/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+
   </div>
 </template>
 
